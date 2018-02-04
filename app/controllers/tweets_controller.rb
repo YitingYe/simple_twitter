2ca765f8fcec78aff636e58fb8fbaae54808c6d8
eating <sstar0403@gmail.com>
2018-02-05 00:57:13 +0800

    setup new action for user and add form on index page for tweet new thing

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,29 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.all
+    @tweet = Tweet.new
     @user_name = current_user.name
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = "Tweet was successfully created"
+      redirect_to tweets_path
+    else
+      flash.now["alert"] = "Restaurant was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
   end
   
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,3 @@
 class Tweet < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user, dependent: :destroy, optional: true
 end
