83f071393d0771fff817506f568ce2415ca7033a
eating <sstar0403@gmail.com>
2018-02-06 02:02:58 +0800

    setup routes for user's tweets pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user do
-    resources :tweets
+    member do
+      get :tweets
+    end
   end
   resources :tweets, only: [:index, :show, :new, :create] do
     resources :replies, only: [:index, :create, :destroy]
