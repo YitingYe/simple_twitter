ac148fac5fd5156c17d34b8833bf0efaf8f9bfff
eating <sstar0403@gmail.com>
2018-02-07 21:02:11 +0800

    add a link to admin panel for admin

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,6 +1,6 @@
 class Followship < ApplicationRecord
   belongs_to :user
-  belongs_to :following, class_name: "User"
+  belongs_to :following, class_name: "User",counter_cache: true
 
   validates :following_id, uniqueness: { scope: :user_id }
 
